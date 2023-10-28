e3345f684f5b1a83fdb311d680ee7fdc8f15816f
Haziel Ortiz <ya-yo22124@outlook.com>
2023-10-28 15:17:22 -0600

    Add: History component

---
+++ src/app/modulos/chats/components/history/history.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-history',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './history.component.html',
+  styleUrls: ['./history.component.scss']
+})
+export class HistoryComponent {
+
+}
