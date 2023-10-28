53fb2be9ea2d9042fabb93848522ee3ee6e7acc8
Haziel Ortiz <ya-yo22124@outlook.com>
2023-10-28 11:04:49 -0600

    Add: Components (NavBar ,Footer)

---
+++ src/app/componentes/nav-bar/nav-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss']
+})
+export class NavBarComponent {
+
+}
