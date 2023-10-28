940f9c250fbf868b6075d0ffe7f80a69290d59cd
Haziel Ortiz <ya-yo22124@outlook.com>
2023-10-28 16:46:34 -0600

    Add: Chats component

---
+++ src/app/modulos/chats/components/chats/chats.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-chats',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './chats.component.html',
+  styleUrls: ['./chats.component.scss']
+})
+export class ChatsComponent {
+
+}
