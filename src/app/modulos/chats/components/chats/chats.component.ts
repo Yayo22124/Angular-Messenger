3fc6b2d30786936f6aefb6bfd3cb119e66623f32
Haziel Ortiz <ya-yo22124@outlook.com>
2023-10-28 17:23:32 -0600

    Upd: Chats styles, structure and input data

--- src/app/modulos/chats/components/chats/chats.component.ts
+++ src/app/modulos/chats/components/chats/chats.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +10,20 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./chats.component.scss']
 })
 export class ChatsComponent {
+  @Input() name: string = "";
+  @Input() lastMessage: string = "";
+  @Input() image: string = "";
+  @Input() history: boolean = false;
+  @Input() status: boolean = false;
+  @Input() seen: boolean = false;
 
+  limitWords(text: string, limit: number): string {
+    const words = text.split(' ');
+    if (words.length > limit) {
+      return words.slice(0, limit).join(' ') + '...';
+    } else {
+      return text;
+    }
+  }
+  
 }
--- src/app/modulos/chats/pages/home/home.component.ts
+++ src/app/modulos/chats/pages/home/home.component.ts
@@ -13,7 +13,7 @@ export class HomeComponent {
       status: false,
       lastMessage:
         'Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
-      seen: false,
+      seen: true,
       history: false,
     },
     {
