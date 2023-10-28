8750085e5cc052bffa2d55ad86810f790b535c0b
Haziel Ortiz <ya-yo22124@outlook.com>
2023-10-28 13:58:48 -0600

    Add: Toolbar Component for bottom toolbar

---
+++ src/app/componentes/tool-bar/tool-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tool-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tool-bar.component.html',
+  styleUrls: ['./tool-bar.component.scss']
+})
+export class ToolBarComponent {
+
+}
