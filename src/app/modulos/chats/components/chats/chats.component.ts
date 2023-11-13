e957b8e728e09dcf6a9663edf36a8b3e1d4fbb09
Haziel Ortiz <ya-yo22124@outlook.com>
2023-11-13 00:11:50 -0600

    Upd: Configure snackbar and seting it

--- src/app/modulos/chats/components/chats/chats.component.ts
+++ src/app/modulos/chats/components/chats/chats.component.ts
@@ -1,27 +1,48 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
+import { IUsuario } from 'src/app/core/interfaces/usuario.interface';
 
 @Component({
   selector: 'app-chats',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './chats.component.html',
-  styleUrls: ['./chats.component.scss']
+  styleUrls: ['./chats.component.scss'],
 })
 export class ChatsComponent {
-  @Input() name: string = "";
-  @Input("last-message") lastMessage: string = "";
-  @Input() image: string = "";
+  @Input() name: string = '';
+  @Input('last-message') lastMessage: string = '';
+  @Input() image: string = '';
   @Input() history: boolean = false;
   @Input() status: boolean = false;
   @Input() seen: boolean = false;
 
-  @Output() actionSeen = new EventEmitter<any>();
+  @Output() actionSeen = new EventEmitter<IUsuario>();
+  @Output() actionSeenHistory = new EventEmitter<any>();
 
-  onChangeSeen(){
-    if (this.seen) return
-    this.seen = !this.seen
+  onChangeSeenHistory() {
+    if (!this.history) return;
+    this.history = !this.history;
+    this.actionSeenHistory.emit({
+      name: this.name,
+      image: this.image,
+      history: this.history,
+      status: this.status,
+    });
+  }
+
+  onChangeSeen() {
+    if (this.seen) return;
+    this.seen = !this.seen;
+    this.actionSeen.emit({
+      name: this.name,
+      lastMessage: this.lastMessage,
+      image: this.image,
+      history: this.history,
+      status: this.status,
+      seen: this.seen,
+    });
   }
 
   limitWords(text: string, limit: number): string {
@@ -32,5 +53,4 @@ export class ChatsComponent {
       return text;
     }
   }
-  
 }
